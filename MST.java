423c21f874f716d123302d32b127b79ddb96db63
Sam Tso <43475551+samtso359@users.noreply.github.com>
2019-07-16 13:53:58 -0400

    Add files via upload

---
+++ MST.java
@@ -0,0 +1,126 @@
+package apps;
+
+import structures.*;
+import structures.Vertex.Neighbor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import apps.PartialTree.Arc;
+
+public class MST {
+	
+	/**
+	 * Initializes the algorithm by building single-vertex partial trees
+	 * 
+	 * @param graph Graph for which the MST is to be found
+	 * @return The initial partial tree list
+	 */
+public static PartialTreeList initialize(Graph graph) {
+	PartialTreeList L = new PartialTreeList(); 
+	
+	for (int i = 0; i<graph.vertices.length; i++){ 
+		Vertex V = graph.vertices[i];
+		PartialTree T = new PartialTree(V); 
+		
+	
+	Vertex.Neighbor N = graph.vertices[i].neighbors;
+	
+	MinHeap<PartialTree.Arc> P = T.getArcs();
+	
+	while (N != null){
+	
+	PartialTree.Arc D = new PartialTree.Arc(graph.vertices[i], N.vertex , N.weight);
+		
+	P.insert(D);
+
+	N = N.next;
+	
+
+	}
+		
+		
+		L.append(T); 
+	}
+	
+
+
+	
+		return L;
+			
+}
+
+
+
+/**
+ * Executes the algorithm on a graph, starting with the initial partial tree list
+ * 
+ * @param graph Graph for which MST is to be found
+ * @param ptlist Initial partial tree list
+ * @return Array list of all arcs that are in the MST - sequence of arcs is irrelevant
+ */
+public static ArrayList<PartialTree.Arc> execute(PartialTreeList ptlist) {
+	
+	/* COMPLETE THIS METHOD */
+	ArrayList<PartialTree.Arc> result = new ArrayList<PartialTree.Arc>();
+	
+	int counter = ptlist.size();
+	
+	
+	while(counter>1)	{
+		
+
+
+		PartialTree PTX = ptlist.remove();
+//		
+		MinHeap<PartialTree.Arc> PQX = PTX.getArcs();
+		
+		PartialTree.Arc highestP= PQX.deleteMin();
+
+		
+		
+	System.out.println("The v2.getRoot() is :"+highestP.v2.getRoot());
+		//while( highestP.v1.equals(highestP.v2) || highestP.v1.equals(highestP.v2.parent)){
+		while(highestP.v2.getRoot().equals(PTX.getRoot())){	
+			System.out.println("PTX.getRoot(): "+PTX.getRoot()+" (v1: "+highestP.v1+", v1parent "+highestP.v1.parent+") (v2: "+highestP.v2+" v2.parent: "+highestP.v2.parent+")");
+			System.out.println("bad highestP, choosing a new one");
+			highestP = PQX.deleteMin();
+			System.out.println(highestP);
+		
+			System.out.println("highestp.v2.parent is: "+highestP.v2);
+
+			
+			System.out.println("New PTX.getRoot(): "+PTX.getRoot()+" (v1: "+highestP.v1+", v1parent "+highestP.v1.parent+") (v2: "+highestP.v2+" v2.parent: "+highestP.v2.parent+")");
+			System.out.println("");
+			
+			
+		}
+		System.out.println("");
+	
+		System.out.println("final target is: "+highestP);
+		System.out.println("PTX.getRoot(): "+PTX.getRoot()+" (v1: "+highestP.v1+", v1parent "+highestP.v1.parent+") (v2: "+highestP.v2+" v2.parent: "+highestP.v2.parent+")");
+		result.add(highestP);
+		
+		PartialTree PTY = ptlist.removeTreeContaining(highestP.v2);
+		
+		System.out.println("PTY is: "+PTY);
+		if(PTY==null){
+			System.out.println("PTY is null");
+			continue;
+		}
+		
+		
+		PTX.merge(PTY);
+		ptlist.append(PTX);
+	
+
+		counter--;
+		System.out.println("_____________________________________________________________________");
+
+}
+
+	return result;
+}
+}
---
+++ PartialTreeList.java
@@ -0,0 +1,216 @@
+package apps;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import structures.Vertex;
+
+
+public class PartialTreeList implements Iterable<PartialTree> {
+    
+	/**
+	 * Inner class - to build the partial tree circular linked list 
+	 * 
+	 */
+	public static class Node {
+		/**
+		 * Partial tree
+		 */
+		public PartialTree tree;
+		
+		/**
+		 * Next node in linked list
+		 */
+		public Node next;
+		
+		/**
+		 * Initializes this node by setting the tree part to the given tree,
+		 * and setting next part to null
+		 * 
+		 * @param tree Partial tree
+		 */
+		public Node(PartialTree tree) {
+			this.tree = tree;
+			next = null;
+		}
+	}
+
+	/**
+	 * Pointer to last node of the circular linked list
+	 */
+	private Node rear;
+	
+	/**
+	 * Number of nodes in the CLL
+	 */
+	private int size;
+	
+	/**
+	 * Initializes this list to empty
+	 */
+    public PartialTreeList() {
+    	rear = null;
+    	size = 0;
+    }
+
+    /**
+     * Adds a new tree to the end of the list
+     * 
+     * @param tree Tree to be added to the end of the list
+     */
+    public void append(PartialTree tree) {
+    	Node ptr = new Node(tree);
+    	if (rear == null) {
+    		ptr.next = ptr;
+    	} else {
+    		
+    		
+    		ptr.next = rear.next;
+    		rear.next = ptr;
+    	}
+    	rear = ptr;
+    	size++;
+  
+    }
+
+    /**
+     * Removes the tree that is at the front of the list.
+     * 
+     * @return The tree that is removed from the front
+     * @throws NoSuchElementException If the list is empty
+     */
+    public PartialTree remove() 
+    	    throws NoSuchElementException {
+    	    	if (rear == null){
+    	  		  throw new NoSuchElementException();
+    	  	  }  
+    	    	if(rear == rear.next){
+    	    		  PartialTree tree = rear.tree;
+    	    		  rear = null;
+    	    		  size--;
+    	    		  return tree;
+    	    	  }
+    	    	if (rear.next != null){
+    	  
+    	    		PartialTree tree = rear.next.tree;
+    	    		rear.next = rear.next.next;
+    	    		size--;
+    	    		return tree;	
+    	  	  }
+    	  	   return null;
+    	  }
+    	    	
+    	    	
+
+    	    /**
+    	     * Removes the tree in this list that contains a given vertex.
+    	     * 
+    	     * @param vertex Vertex whose tree is to be removed
+    	     * @return The tree that is removed
+    	     * @throws NoSuchElementException If there is no matching tree
+    	     */
+    	    public PartialTree removeTreeContaining(Vertex vertex) 
+    	    throws NoSuchElementException {
+    	    		
+    			if (rear == null) { 
+    				throw new NoSuchElementException("List is empty");
+    			}
+    			if (rear == rear.next) { 
+    			      if (vertex.equals(rear.tree.getRoot())) { 
+    			    	  PartialTree PTY = rear.tree;
+    			    	  rear = null;
+    			    	  size--;
+    			         return PTY;
+    			      } 
+    			}
+    	        
+
+    			   Node prev=rear, curr=rear.next;
+    			   
+    			   do{
+    	
+    		    		System.out.println(curr.tree.getRoot()+" is being compare to "+vertex);
+    			       if (vertex.equals(curr.tree.getRoot())||vertex.parent.equals(curr.tree.getRoot())){
+    			    	  System.out.println("Target found");
+    			    	 
+    			    	   	PartialTree PTX = curr.tree;
+    			    	   	System.out.println("curr.tree is: "+curr.tree);
+    			             prev.next = curr.next;
+    			             if (curr == rear) { 
+    			                rear = prev;
+    			             }
+    			             
+    			             size--;
+    			             return PTX;
+    			       } 
+    		
+    			       prev = curr;
+    			       curr = curr.next;
+    			   } while (prev != rear);
+    			   
+    			   if(curr==rear && prev==rear ){
+    					throw new NoSuchElementException("no match");
+    			   }
+    			
+    			   
+    	  	   	PartialTree PTX = rear.tree;
+    	  	   	size--;
+    	  	   	return PTX;
+    			
+    	   }
+	    
+    
+    /**
+     * Gives the number of trees in this list
+     * 
+     * @return Number of trees
+     */
+    public int size() {
+    	return size;
+    }
+    
+ 
+    /**
+     * Returns an Iterator that can be used to step through the trees in this list.
+     * The iterator does NOT support remove.
+     * 
+     * @return Iterator for this list
+     */
+    public Iterator<PartialTree> iterator() {
+    	return new PartialTreeListIterator(this);
+    }
+    
+    private class PartialTreeListIterator implements Iterator<PartialTree> {
+    	
+    	private PartialTreeList.Node ptr;
+    	private int rest;
+    	
+    	public PartialTreeListIterator(PartialTreeList target) {
+    		rest = target.size;
+    		ptr = rest > 0 ? target.rear.next : null;
+    	}
+    	
+    	public PartialTree next() 
+    	throws NoSuchElementException {
+    		if (rest <= 0) {
+    			throw new NoSuchElementException();
+    		}
+    		PartialTree ret = ptr.tree;
+    		ptr = ptr.next;
+    		rest--;
+    		return ret;
+    	}
+    	
+    	public boolean hasNext() {
+    		return rest != 0;
+    	}
+    	
+    	public void remove() 
+    	throws UnsupportedOperationException {
+    		throw new UnsupportedOperationException();
+    	}
+    	
+    }
+}
+
+
